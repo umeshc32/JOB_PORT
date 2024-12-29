8dd3eb1f91c56f06d99c330daa03075865bec673
Umesh Cirvi <umeshcirvi3232@gmail.com>
2024-12-29 23:19:34 +0530

    Production ready done

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://job-port-eg5l.onrender.com',
     credentials:true
 }
 
